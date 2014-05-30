ea350754bba05f5e029076e0fe869c13fd876165
Viktor Kirilov <deepsybg@gmail.com>
2014-05-31 02:38:37 +0300

    Favicon, transitions, cleanup, clever title and more

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-var App = angular.module('Fobos', ['ngResource', 'ui.router']);
+var App = angular.module('Fobos', ['ngResource', 'ui.router', 'ngAnimate']);
 
 App.config(
 	['$stateProvider', '$urlRouterProvider', '$locationProvider',
@@ -11,27 +11,32 @@ App.config(
 			$stateProvider
 				.state("home", {
 					url: "/",
-					templateUrl: '/partials/home.html'
+					templateUrl: '/partials/home.html',
+					title: 'Начало'
 				})
 				.state("products", {
 					url: "/products",
 					templateUrl: '/partials/products.html',
-					controller: 'ProductCtrl'
+					controller: 'ProductCtrl',
+					title: 'Продукти'
 				})
 				.state("products.category", {
 					url: "/cat/:id",
 					controller: 'CategoryCtrl',
-					templateUrl: '/partials/products.template.html'
+					templateUrl: '/partials/products.template.html',
+					title: 'Продукти'
 				})
 				.state("products.view", {
 					url: "/:id",
 					controller: 'ViewProductCtrl',
-					templateUrl: '/partials/products.single.html'
+					templateUrl: '/partials/products.single.html',
+					title: 'Продукти'
 				})
 				.state("interesting", {
 					url: "/interesting",
 					templateUrl: '/partials/interesting.html',
-					controller: 'InterestCtrl'
+					controller: 'InterestCtrl',
+					title: 'Интерестно'
 				})
 				.state("interesting.page", {
 					url: "/:id",
@@ -39,19 +44,76 @@ App.config(
 						function($rootScope, $stateParams) {
 							$rootScope.$emit('articleid', $stateParams.id)
 						}
-					]
+					],
+					title: 'Интерестно'
 				})
 				.state('about', {
 					url: '/about',
-					templateUrl: '/partials/about.html'
+					templateUrl: '/partials/about.html',
+					title: 'За нас'
 				})
 				.state('contacts', {
 					url: '/contacts',
 					templateUrl: '/partials/contacts.html',
-					controller: 'ContactCtrl'
+					controller: 'ContactCtrl',
+					title: 'Контакти'
 				});
 
 			$locationProvider.html5Mode(true);
 		}
 	]
 );
+
+App.run(['$rootScope', '$state',
+	function($rootScope, $state) {
+		$rootScope.$on("$stateChangeSuccess", function(currentRoute, previousRoute) {
+			$rootScope.$state = $state;
+			console.log($state);
+		});
+	}
+]);
+
+App.factory('API', ['$resource',
+	function($resource) {
+		return {
+			getInteresting: function() {
+				return $resource('/api/interesting.json');
+			},
+			getProducts: function() {
+				return $resource('/api/products.json');
+			}
+		}
+	}
+]);
+
+App.filter('dots', function() {
+	return function(input) {
+		input = input.substr(0, 80) + '..';
+		return input;
+	}
+});
+
+App.filter('category', function() {
+	return function(items, cat) {
+		var filtered = [];
+		angular.forEach(items, function(item) {
+			if (item.category == cat) {
+				filtered.push(item);
+			}
+		});
+		return filtered;
+	};
+});
+
+App.directive('toggler', function() {
+	return function(scope, element) {
+		var rightMenu = document.getElementById('mainmenu');
+		element[0].addEventListener('click', function() {
+			if (rightMenu.className == 'showed') {
+				rightMenu.className = '';
+			} else {
+				rightMenu.className = 'showed';
+			}
+		}, false);
+	}
+});
