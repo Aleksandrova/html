021f86a61c6787e6de833ec8b9444cc396dcdead
Viktor Kirilov <deepsybg@gmail.com>
2014-06-05 00:21:40 +0300

    ad

--- old/views/contacts.php
+++ old/views/contacts.php
@@ -1,21 +1,21 @@
 <div class="dark-holder-small" id="contacts-holder">
 	<div style="overflow: hidden;">
 		<div id="left-part" class="part">
-			<a href="tel:+35996300314">
+			<a class="bg-hack" href="tel:+35996300314">
 				<img src="/static/img/icons/phone.png" align="left">
 				<p>096/300 314</p>
 			</a>
-			<a href="tel:+35996313581">
+			<a class="bg-hack" href="tel:+35996313581">
 				<img src="/static/img/icons/phone.png" align="left">
 				<p>096/313 581</p>
 			</a>
 		</div>
 		<div id="right-part" class="part">
-			<a href="tel:+359888476358">
+			<a class="bg-hack" href="tel:+359888476358">
 				<img src="/static/img/icons/mobile.png" align="left">
 				<p>0888 476 358</p>
 			</a>
-			<a href="mailto: fobosr@gmail.com">
+			<a class="bg-hack" href="mailto: fobosr@gmail.com">
 				<img src="/static/img/icons/mail.png" align="left">
 				<p>fobosr@gmail.com</p>
 			</a>
--- old/views/index.php
+++ old/views/index.php
@@ -4,16 +4,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="/static/css/style.css">
-	<link rel="stylesheet" type="text/css" href="/static/css/pages.css">
 	<title>Начало - Фобос ЕР</title>
 	<meta name="fragment" content="!" />
 	<!--[if IE 8]><!--> <meta http-equiv="X-UA-Compatible" content="IE=8" /> <!--<![endif]-->
 </head>
-<body ng-controller="BaseCtrl">
-	<header>
+<body>
+	<div class="header">
 		<div id="holder">
 			<div id="logo">
-				<a href="/"> <img src="/static/img/logo.png"> </a>
+				<a href="<?=$prefix;?>/"> <img src="/static/img/logo.png"> </a>
 			</div>
 
 			<img src="/static/img/lines.png" id="lines" class="show-for-small toggle-icon">
@@ -28,7 +27,7 @@
 			
 			<div style="clear: both;"></div>
 		</div>
-	</header>
+	</div>
 
 	<div id="content-wrapper">
 		<div class="well">
