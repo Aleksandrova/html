df967e00957ef29ae16f2ec7ac896a68477fa595
Aleksandrova <ahaa96@abv.bg>
2014-06-14 01:50:06 +0300

    404 Error page

--- views/404.php
+++ views/404.php
@@ -1,1 +1,7 @@
-asdasdad
+<div id="holder-404" >
+	<div id="image-404"><img width="100%" src="/static/img/404 copy.png"></div>
+	<div id="text-404">
+		<p>Съжаляваме!</p>
+		<p>Страницата не беше намерена.</p>
+	</div>
+</div>
